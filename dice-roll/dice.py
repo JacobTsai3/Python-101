fcc9ec501cc810de64e3506ded6dac7808e31a7e
JacobTsai3 <jacob3tsai@gmail.com>
2022-07-22 13:03:32 -0700

    random dice roll
    Co-authored-by: Sarah Guthals <drguthals@users.noreply.github.com>

--- dice-roll/dice.py
+++ dice-roll/dice.py
@@ -1,2 +1,16 @@
-num = 3
-print(num)
+import random
+
+# Create a variable to track the value of the die
+dice_val = 0
+
+# Create a variable to track minimum value of the die
+min_val = 1
+
+# Create a variable to track maximum value of the die
+max_val = 6
+
+# Create a random number between min_val and max_val
+dice_val = random.randint(min_val, max_val)
+
+# Print the value of the dice roll
+print(dice_val)
