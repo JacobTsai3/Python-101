e1d5c69ca12e7a1ab8d500b825e2af6380bc87b9
JacobTsai3 <jacob3tsai@gmail.com>
2022-07-26 12:04:22 -0700

    rock paper scissors hw

---
+++ week1/rock-paper-scissors.py
@@ -0,0 +1,11 @@
+# `name` has the value "Jacob", which is of type String
+name = "Jacob"
+
+# `favorite_number` has the value 13, which is of type integer, often called int
+favorite_number = 13
+
+# A function `print` is called, with the parameter "Your name is " is passed in
+print("Your name is ")
+
+# A function `print` is called, with the parameter `name`, which has the value "Jacob" is passed in
+print(name)
