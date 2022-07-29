9890d73612d28fc6c320ac1c7544920a6abe6399
JacobTsai3 <jacob3tsai@gmail.com>
2022-07-28 23:11:48 -0700

    calculator

--- week1/calculator.py
+++ week1/calculator.py
@@ -16,6 +16,18 @@ def add(number1, number2) :
     sum = number1 + number2
     print (number1, "and", number2, "added =", sum)
 
+def subtract(number1, number2) :
+    total = number1 - number2
+    print (number1, "subtracted by", number2, "=", total)
+
+def multiply(number1, number2) :
+    product = number1 * number2
+    print (number1, "multiplied by", number2, "=", product)
+
+def divide(number1, number2) :
+    quotient = number1 / number2
+    print (number1, "divided by", number2, "=", quotient)
+
 print("Welcome to my calculator!")
 operation = input("Choose an operation: add, subtract, multiply, divide, negate: ")
 
@@ -27,3 +39,19 @@ if operation == "add":
     number1 = input("Give me the first number to add ")
     number2 = input("Give me the second number to add ")
     add (int(number1), int(number2))
+
+
+if operation == "subtract":
+    number1 = input("Give me a number to subtract from ")
+    number2 = input("How much would you like to subtract? ")
+    subtract (int(number1), int(number2))
+
+if operation == "multiply":
+    number1 = input("Give me the first number to multiply ")
+    number2 = input("Give me the second number to multiply ")
+    multiply (int(number1), int(number2))
+
+if operation == "divide":
+    number1 = input("Give me the first number to divide ")
+    number2 = input("How much would you like to divide by? ")
+    multiply (int(number1), int(number2))
